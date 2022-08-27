975fe0c01096326dc969a96480114b351a76bc96
PhilippePerret <philippe.perret@yahoo.fr>
2022-08-27 06:59:12 +0200

    Mise en place de la ZManager class

---
+++ js/system/ZManager.js
@@ -0,0 +1,15 @@
+'use strict'
+/**
+* Class ZManager
+* --------------
+* Gestion des annulations
+* 
+* Courant :
+*   Faire une classe qui puisse fonctionner pour n'importe quelle
+*   application en définissant certaines conventions.
+* 
+*/
+
+class ZManager {
+
+}
